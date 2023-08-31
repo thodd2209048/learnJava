eaee60c7f9d0054c0f6e9e71d6d840e647036ec4
thodd2209048 <114908027+thodd2209048@users.noreply.github.com>
2023-08-31 16:26:47 +0700

    update server address

--- practiceJava/UserPostReaction/src/main/java/org/example/CreateSampleRows.java
+++ practiceJava/UserPostReaction/src/main/java/org/example/CreateSampleRows.java
@@ -10,23 +10,19 @@ public class CreateSampleRows {
         Class.forName("org.postgresql.Driver");
         Connection connection = DriverManager
                 .getConnection(
-                        "jdbc:postgresql://localhost:5432/UserPostReaction",
-                        "postgres",
-                        "123"
-
-//                        "jdbc:postgresql://4.194.217.58:5432/t2301e",
-//                        "t2301e",
-//                        "t2301e"
+                        "jdbc:postgresql://4.194.217.58:5432/t2301e",
+                        "t2301e",
+                        "t2301e"
                 );
         Statement statement = connection.createStatement();
 
-//        statement.executeUpdate("INSERT INTO reactions_thodd (post_id, user_id, type)\n" +
-//                "VALUES\n" +
-//                "    (1, 1, 'LIKE'),\n" +
-//                "    (1, 2, 'LOVE'),\n" +
-//                "    (2, 3, 'DISLIKE'),\n" +
-//                "    (2, 1, 'CRY'),\n" +
-//                "    (3, 4, 'LIKE');");
+        statement.executeUpdate("INSERT INTO reactions_thodd (post_id, user_id, type)\n" +
+                "VALUES\n" +
+                "    (1, 1, 'LIKE'),\n" +
+                "    (1, 2, 'LOVE'),\n" +
+                "    (2, 3, 'DISLIKE'),\n" +
+                "    (2, 1, 'CRY'),\n" +
+                "    (3, 4, 'LIKE');");
 
         statement.executeUpdate("INSERT INTO posts_thodd (user_id, published_at, content)\n" +
                 "VALUES\n" +
@@ -36,13 +32,13 @@ public class CreateSampleRows {
                 "    (1, '2023-08-30 16:20:00', 'Nội dung bài viết 4'),\n" +
                 "    (4, '2023-08-30 17:10:00', 'Nội dung bài viết 5');");
 
-//        statement.executeUpdate("INSERT INTO users_thodd (first_name, last_name, dob)\n" +
-//                "VALUES\n" +
-//                "    ('John', 'Doe', '1990-05-15'),\n" +
-//                "    ('Jane', 'Smith', '1985-12-10'),\n" +
-//                "    ('Michael', 'Johnson', '1992-08-20'),\n" +
-//                "    ('Emily', 'Williams', '1998-03-25'),\n" +
-//                "    ('David', 'Brown', '1982-11-03');");
+        statement.executeUpdate("INSERT INTO users_thodd (first_name, last_name, dob)\n" +
+                "VALUES\n" +
+                "    ('John', 'Doe', '1990-05-15'),\n" +
+                "    ('Jane', 'Smith', '1985-12-10'),\n" +
+                "    ('Michael', 'Johnson', '1992-08-20'),\n" +
+                "    ('Emily', 'Williams', '1998-03-25'),\n" +
+                "    ('David', 'Brown', '1982-11-03');");
 
         statement.close();
         connection.close();
--- practiceJava/UserPostReaction/src/main/java/org/example/CreateTable.java
+++ practiceJava/UserPostReaction/src/main/java/org/example/CreateTable.java
@@ -10,30 +10,30 @@ public class CreateTable {
         Class.forName("org.postgresql.Driver");
         Connection connection = DriverManager
                 .getConnection(
-                        "jdbc:postgresql://localhost:5432/UserPostReaction",
-                        "postgres",
-                        "123"
+                        "jdbc:postgresql://4.194.217.58:5432/t2301e",
+                        "t2301e",
+                        "t2301e"
                 );
         Statement statement = connection.createStatement();
-//        statement.executeUpdate("create table users_thodd\n" +
-//                "(\n" +
-//                "    id         serial,\n" +
-//                "    first_name varchar not null,\n" +
-//                "    last_name  varchar not null,\n" +
-//                "    dob        date    not null,\n" +
-//                "    constraint users_thodd_pk\n" +
-//                "        primary key (id)\n" +
-//                ");");
+        statement.executeUpdate("create table users_thodd\n" +
+                "(\n" +
+                "    id         serial,\n" +
+                "    first_name varchar not null,\n" +
+                "    last_name  varchar not null,\n" +
+                "    dob        date    not null,\n" +
+                "    constraint users_thodd_pk\n" +
+                "        primary key (id)\n" +
+                ");");
 
-//        statement.executeUpdate("create table posts_thodd\n" +
-//                "(\n" +
-//                "    id           serial,\n" +
-//                "    user_id    integer   not null,\n" +
-//                "    published_at timestamp not null,\n" +
-//                "    content      varchar   not null,\n" +
-//                "    constraint posts_thodd_pk\n" +
-//                "        primary key (id)\n" +
-//                ");");
+        statement.executeUpdate("create table posts_thodd\n" +
+                "(\n" +
+                "    id           serial,\n" +
+                "    user_id    integer   not null,\n" +
+                "    published_at timestamp not null,\n" +
+                "    content      varchar   not null,\n" +
+                "    constraint posts_thodd_pk\n" +
+                "        primary key (id)\n" +
+                ");");
 //
         statement.executeUpdate("CREATE TABLE reactions_thodd (\n" +
                 "    id serial PRIMARY KEY,\n" +
