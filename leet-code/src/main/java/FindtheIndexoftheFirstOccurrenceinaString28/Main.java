8835aaac4bce80e2d2ca5f5cfefb9b24f5004a00
thodd2209048 <114908027+thodd2209048@users.noreply.github.com>
2023-09-26 23:27:11 +0700

    28. Find the Index of the First Occurrence in a String

---
+++ leet-code/src/main/java/FindtheIndexoftheFirstOccurrenceinaString28/Main.java
@@ -0,0 +1,10 @@
+package FindtheIndexoftheFirstOccurrenceinaString28;
+
+public class Main {
+    public static void main(String[] args) {
+        String haystack = "leetcode";
+        String needle = "s";
+        int result = Solution.strStr(haystack, needle);
+        System.out.println(result);
+    }
+}
---
+++ leet-code/src/main/java/FindtheIndexoftheFirstOccurrenceinaString28/Solution.java
@@ -0,0 +1,8 @@
+package FindtheIndexoftheFirstOccurrenceinaString28;
+
+//https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
+public class Solution {
+    public static int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
